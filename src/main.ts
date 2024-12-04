24ea70b1c8da25d29489bc1b71e37f0c2d550191
devsentap <devsentap@gmail.com>
2024-12-04 16:20:19 +0800

    refactor: able to use vue-router to show different pages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
+// import { Quasar } from 'quasar'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import CityList      from './pages/CityList.vue'
+import WeatherDetail from './pages/WeatherDetail.vue'
+import EditProfile   from './pages/EditProfile.vue'
+
+const routes = [
+  { path: '/',              name: 'CityList',      component: CityList },
+  { path: '/weatherdetail', name: 'WeatherDetail', component: WeatherDetail },
+  { path: '/editprofile',   name: 'EditProfile',   component: EditProfile }
+  // TODO: 404 page
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
---
+++ src/pages/WeatherDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Weather Detail</h1>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
