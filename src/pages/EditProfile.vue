57b5ed8053702e27de24b8046c4f3ce4bdae7340
devsentap <devsentap@gmail.com>
2024-12-05 15:00:51 +0800

    build: choose vue3-q-tel-input library for phone number with country

--- src/pages/EditProfile.vue
+++ src/pages/EditProfile.vue
@@ -2,7 +2,18 @@
   <q-layout class="q-pa-lg" style="width: 750px;margin: auto;">
     <q-page-container>
       <q-page>
-        <h1>Edit Profile</h1>
+        <div class="row"></div>
+        <div class="text-h3">Edit Profile</div>
+        <q-input v-model="text" label="Full Name" stack-label />
+        <q-input v-model="text" label="Email" stack-label />
+        <!-- <q-input v-model="text" label="Phone Number">
+          <template v-slot:prepend>
+            <q-icon name="event" />
+          </template>
+        </q-input> -->
+        <!-- <q-tel-input></q-tel-input> -->
+        <vue3-q-tel-input v-model="tel" label="Phone Number" stack-label />
+        <VuePhoneNumberInput v-model="yourValue" />
       </q-page>
     </q-page-container>
   </q-layout>
